67fe6b5c18b32dea470abad4f475b68dd4d32767
Iskander <isk.dev001@gmail.com>
2023-10-31 12:52:27 +0600

    Added header and assets

---
+++ components/TheHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="bg-white">
+    <div class="container mx-auto flex items-center justify-between py-4">
+      <NuxtLink to="/"><img class="w-16" src="/logo.png" alt="logo"></NuxtLink>
+      <button class="border-2 border-black p-2 rounded-lg"><img src="/theme.svg" alt="theme"></button>
+    </div>
+  </header>
+</template>
