7b8e8ca688dd79b48f2234eb031c59ab49a76088
Iskander <isk.dev001@gmail.com>
2023-11-02 17:00:45 +0600

    Added nuxt-icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
       "nuxt-vuefire",
       "@pinia/nuxt",
       "nuxt-headlessui",
-      "@nuxtjs/tailwindcss"
+      "@nuxtjs/tailwindcss",
+      "nuxt-icon"
   ],
     vuefire: {
         config: {
