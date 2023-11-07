9c8edf4c5e32ea84f137e44f09f27b7f454f3a1f
Iskander <isk.dev001@gmail.com>
2023-11-07 15:09:12 +0600

    Added error search message

---
+++ components/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const characterStore = useCharacterStore()
+const {searchQuery} = storeToRefs(characterStore)
+</script>
+
+<template>
+  <section>
+    <div class="flex flex-col gap-4 items-center mt-10">
+      <h1 class="font-bold text-3xl">Nothing found</h1>
+      <p class="text-gray-500">You searched for "{{ searchQuery }}"</p>
+    </div>
+  </section>
+</template>
